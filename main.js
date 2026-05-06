0f452ce39c41dc950ab9c438aca336c07e7558f9
Tech_Nisha <nishasahu.tech@gmail.com>
2026-05-06 12:23:07 +0530

    main.js

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-const navbar =document.querySelector(".navbar");
+const navbar = document.querySelector(".navbar");
 
-window.addEventListener("scroll", ()=>{
-  if(window.scrollY>0){
+window.addEventListener("scroll", () => {
+ 
+  if (window.scrollY > 50) {
     navbar.classList.add("scroll-nav");
-  }
-  else{
+  } else {
     navbar.classList.remove("scroll-nav");
   }
-})
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    const targetId = this.getAttribute("href");
+    if (targetId !== "#" && targetId.startsWith("#")) {
+      const targetElement = document.querySelector(targetId);
 
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
+      if (targetElement) {
+        e.preventDefault();     
+       targetElement.scrollIntoView({
+          behavior: "smooth",
+          block: "start"
+        });
+      }
+    }
   });
 });
